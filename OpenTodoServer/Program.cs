779f4cd236d58ad3961cdf2bf5ffea7235a85183
XIYUEKONGLING <229533969+XIYUEKONGLING@users.noreply.github.com>
2025-12-16 10:48:56 +0800

    Initialize project

---
+++ OpenTodoServer/Program.cs
@@ -0,0 +1,28 @@
+namespace OpenTodoServer;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.Run();
+
+        return 0;
+    }
+}
